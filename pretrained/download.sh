d2ccb5bfde76cf93cbe86e9393ccdc3f79d2baa3
GuilhermeCunha <guiscunha@gmail.com>
2020-04-07 16:01:42 -0300

    Changing script to not need the 'pup' command

--- pretrained/download.sh
+++ pretrained/download.sh
@@ -1,16 +1,42 @@
+#!/bin/bash
+ROOTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-echo 'Downloading pretrained TSN weights for Flow on Kinetics...'
-echo
-echo
+CUSTOM_PATH=$1
+if [ "$#" -eq 1 ]; then
+    outputPath="$CUSTOM_PATH"
+else
+    outputPath="$ROOTPATH"
+fi
 
-wget "https://docs.google.com/uc?export=download&id=1vO_vALz8HXifZaEgtLBvtEq869AKf6kc" -O "kinetics_tsn_flow.pth.tar" 
+if ! [ -x "$(command -v wget)" ]; then
+    echo "Error!: wget is not installed! Please install it and try again"
+    exit 1
+fi
 
-echo 'Downloading pretrained TBN weights for RGB-Flow-Audio on EPIC-Kitchens...'
-echo 
-echo
+echo -e "\n### ------------------------------------------------------- ###\n"
+echo "### Downloading into $outputPath"
+echo -e "\n### ------------------------------------------------------- ###\n"
 
-query=`curl -c ./cookie.txt -s -L "https://drive.google.com/uc?export=download&id=1c2z0xrshfpLvhcbkIpNJVcdyPe5rEO-g" | pup 'a#uc-download-link attr{href}' | sed -e 's/amp;//g'`
+GOOGLE_DRIVE_API_URL="https://docs.google.com/uc?export=download&id="
 
-curl -b ./cookie.txt -L -o "epic_tbn_rgbflowaudio.pth.tar" "https://drive.google.com${query}"
+download_() {
+    
+    local file_id=$1
+    local path=$2
+    local file_name=$3
+    local internal_url="https://docs.google.com/uc?export=download&id=$file_id"
+    local final_url="https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate $internal_url -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=$file_id"
+    echo "final_url: $final_url"
+    echo -ne "# Downloading "$file_name"\t"
+    wget --load-cookies /tmp/cookies.txt $final_url --directory-prefix="$path" -O $file_name && rm -rf /tmp/cookies.txt
+    echo -ne "\b\b\b\b"
+    echo " # Done"
+}
 
-rm "cookie.txt"
+# download_ FILEID PATHDORESULTADO NOMEDOARQUIVO
+download_ 1vO_vALz8HXifZaEgtLBvtEq869AKf6kc "$outputPath" "kinetics_tsn_flow.pth.tar"
+download_ 1c2z0xrshfpLvhcbkIpNJVcdyPe5rEO-g "$outputPath" "epic_tbn_rgbflowaudio.pth.tar"
+
+echo -e "\n### ------------------------------------------------------- ###\n"
+echo "### All Done"
+echo -e "\n### ------------------------------------------------------- ###\n"
